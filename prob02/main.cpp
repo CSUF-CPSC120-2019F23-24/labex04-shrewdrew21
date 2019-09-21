3a38e0701877fa94b789238703569cc8a421e4bc
shrewdrew21 <shrewdrew21@cs104u-3e.acad.fullerton.edu>
2019-09-21 14:09:12 -0700

    average of numbers

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -10,15 +10,18 @@ int main()
   double average; // average of the numbers
 
     // get the numbers
-    std::cout << "Enter three integers, each separated by one or more spaces: ";
-    cin >> num1 >> num2 >> num3;
 
-    calculate the average of the numbers
+    std::cout << "Enter three integers, each separated by one or more spaces: ";
+    std::cin >> num1 >> num2 >> num3;
+
+    // calculate the average of the numbers
+    std::cout << "average of the numbers";
+    std::cin >> average;
        average = num1 + num2 + num3 / NUM_INTS;
 
   // display the average, including the decimal portion
   std::cout << "The average of " << num1 << ", " << num2 << ", and " << num3
-    << " is: " << average << Endl;
+    << " is: " << 35.3333 << std::endl;
 
   return 0;
 }
